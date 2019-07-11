3c2956b46bbcaf9620df0ef75f7d2cd8a58f5877
Cisco89 <franciscoadriangarcia1@gmail.com>
2019-07-11 00:07:49 -0600

    refactored to using the database, added completion check off and completed section in page

--- main.py
+++ main.py
@@ -1,21 +1,57 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,redirect
+import os
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+project_dir = os.path.dirname(os.path.abspath(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(os.path.join(project_dir, "get-it-done.db"))
+app.config['SQLALCHEMY_ECHO'] = True
 
-tasks = []
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    completed = db.Column(db.Boolean)
+
+    def __init__(self, name):
+        self.name = name
+        self.completed = False
 
 @app.route('/', methods=['POST', 'GET'])
-def todos():
+def index():
 
     if request.method == 'POST':
-        task = request.form['task']
-        tasks.append(task)
+        task_name = request.form['task']
+        new_task = Task(task_name)
+        db.session.add(new_task)
+        db.session.commit()
+
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()
+    
 
     return render_template(
         'todos.html', 
         title='Get it Done!', 
         tasks=tasks,
+        completed_tasks=completed_tasks,
         )
 
-app.run()
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect('/')
+
+
+
+if __name__ == '__main__':
+    app.run()
